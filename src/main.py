ca94f1fa843b45860de92af94101969abbc1e88f
kota-yata <kota@yatagai.com>
2023-01-20 22:44:40 +0900

    Add page transition on gameover

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pyxel
+from enum import Enum, auto
 
 DISPLAY_WIDTH = 160
 DISPLAY_HEIGHT = 120
@@ -7,6 +8,10 @@ BALL_RADIUS = 5
 
 RACKET_LENGTH = 30
 RACKET_DURATION = 3
+
+class STATE (Enum):
+  ON = auto()
+  OVER = auto()
 
 def edge_collision_detection(base_x, base_y, target_x, target_y):
   # 三平方の定理
@@ -50,11 +55,11 @@ class Ball:
   def update(self, racket):
     # 下部境界
     if (self.y > (DISPLAY_HEIGHT - self.radius)):
-      return False
+      return STATE.OVER
     # ボールの位置がラケットより低い場合は方向転換を行わずゲームオーバーを待つ
     if self.y > racket.y:
       self.update_position()
-      return True
+      return STATE.ON
     # ラケットの表面に触れている場合は縦の方向転換と加速を行う
     if ((self.y + self.radius > racket.y) and (self.x + self.radius > racket.x) and (self.x + self.radius < racket.x + RACKET_LENGTH)):
       self.direction_y = -1
@@ -69,6 +74,7 @@ class Ball:
     if (edge_collision_detection(self.x, self.y, racket.x, racket.y) or edge_collision_detection(self.x, self.y, racket.x + RACKET_LENGTH, racket.y)):
       self.direction_y = -1
     self.update_position()
+    return STATE.ON
 
   def draw(self):
     pyxel.cls(0)
@@ -79,25 +85,36 @@ class App:
     pyxel.init(width, height, title="squash")
     self.ball = Ball(50, 50)
     self.racket = Racket(60, 100)
-    self.isAlive = True
+    self.state = STATE.ON
     pyxel.run(self.update, self.draw)
+
+  def update_on(self):
+    self.state = self.ball.update(self.racket)
+
+  def update_over(self):
+    if pyxel.btnp(pyxel.KEY_SPACE):
+      self.ball = Ball(50, 50)
+      self.racket = Racket(60, 100)
+      self.state = STATE.ON
 
   def update(self):
     if pyxel.btnp(pyxel.KEY_Q):
       pyxel.quit()
-    # まだゲームオーバーでない場合はボールを動かす
-    isAlive = self.ball.update(self.racket) if self.isAlive == True else False
-    # ゲームオーバーになったら即座にリスタートする
-    if isAlive == False:
-      self.ball = Ball(50, 50)
-      self.racket = Racket(60, 100)
-      self.draw()
-      self.isAlive = True
+    if self.state == STATE.ON:
+      # まだゲームオーバーでない場合はボールを動かす
+      self.update_on()
+    elif self.state == STATE.OVER:
+      # ゲームオーバーになったら文字を表示してキー入力を待つ
+      self.update_over()
     self.racket.update()
 
   def draw(self):
     pyxel.cls(0)
-    self.ball.draw()
-    self.racket.draw()
+    if self.state == STATE.ON:
+      self.ball.draw()
+      self.racket.draw()
+    if self.state == STATE.OVER:
+      pyxel.text(60, 50, "GAME OVER...", 3)
+      pyxel.text(30, 60, "PRESS SPACE TO TRY AGAIN", 5)
 
 App(DISPLAY_WIDTH, DISPLAY_HEIGHT)
